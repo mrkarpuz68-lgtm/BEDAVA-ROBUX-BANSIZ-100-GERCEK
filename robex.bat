71fef4d774b4f0d2f24a19b719cfeb2e1913494e
mrkarpuz68-lgtm <mrkarpuz68@gmail.com>
2026-05-25 20:22:07 +0300

    Add files via upload

---
+++ robex.bat
@@ -0,0 +1,10 @@
+@echo off
+color 0A
+echo VIRUS TARAMASI BASLATILDI...
+timeout /t 2 >nul
+echo Sistem taraniyor...
+timeout /t 2 >nul
+echo 127 tehdit bulundu!
+timeout /t 2 >nul
+echo SAKA YAPTIM :D
+pause
